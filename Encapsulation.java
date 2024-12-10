e6b1640d11b468019825933edce3c6c040cadc10
Psammy <ampadupsamuel1@gmail.com>
2024-12-10 01:07:41 +0000

    Update files

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,2 +1,19 @@
 public class Encapsulation {
+    private String name;
+    private String energy;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String newName){
+        this.name=newName;
+        }
+
+    public static void main(String[] args){
+        Encapsulation objectName=new Encapsulation();
+        objectName.setName("Samuel");
+        System.out.println(objectName.name);
+    }
 }
+
+
