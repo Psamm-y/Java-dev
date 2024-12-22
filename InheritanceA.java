0cef63be164c5da75cfa25eb61d47695318775ac
Psammy <ampadupsamuel1@gmail.com>
2024-12-22 17:32:26 +0000

    InheritanceA file added

---
+++ InheritanceA.java
@@ -0,0 +1,18 @@
+package Javadev;
+
+public class InheritanceA {
+    protected String brandName;
+
+
+    class Car extends InheritanceA {
+        Car myCar = new Car();
+
+        public Car getMyCar() {
+            return myCar;
+        }
+
+
+
+
+    }
+}
