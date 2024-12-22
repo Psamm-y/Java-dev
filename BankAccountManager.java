6a5e35e4ddcfa986d8f67bf6a8a32a8e27340211
Psammy <ampadupsamuel1@gmail.com>
2024-12-22 23:47:15 +0000

    Update BankAccountManager

--- BankAccountManager.java
+++ BankAccountManager.java
@@ -1,9 +1,11 @@
 package Javadev;
 
+import java.sql.SQLOutput;
+
 public class BankAccountManager {
     private String accountNumber;
     private String accountHolder;
-    private int balance;
+    private double balance=0;
     private boolean transaction;
 
     public String getAccountNumber() {
@@ -14,7 +16,7 @@ public class BankAccountManager {
         return accountHolder;
     }
 
-    public int getBalance() {
+    public double getBalance() {
         return balance;
     }
 
@@ -24,7 +26,7 @@ public class BankAccountManager {
 
 
 
-    public void BankAccountManager(String accountNumber,String accountHolder,int balance){
+    public void BankAccountManager(String accountNumber,String accountHolder,double balance){
         this.accountNumber=accountNumber;
         this.accountHolder=accountHolder;
         this.balance=balance;
@@ -36,12 +38,33 @@ public class BankAccountManager {
     System.out.println("Deposit was successful. Your new balance is: GHc"+balance);
     }
 
-    public void withdraw(int amount){
-        balance-=amount;
-        transaction=true;
-        System.out.println("Withdrawal succesful. Your new balance is: GHc"+balance);
+    public void withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            transaction = true;
+            System.out.println("Withdrawal succesful. Your new balance is: GHc" + balance);
+        }else{
+            transaction=false;
+            System.out.println("Withdrawal denied");
+        }
+    }
+
+    public String checkBalance(){
+        return "Your current balance is: "+ balance;
+    }
+
+    public static String options(){
+        return "1.Deposit\n"+
+                "2.Withdraw\n"+
+                "3.Check Balance";
     }
     public static void main(String[] args){
+        BankAccountManager bank= new BankAccountManager();
+        int userChoice;
+        System.out.println("Welcome to your banking system!\n" +
+                "Please choose from the options below");
+        System.out.println(options());
+        if
 
     }
 }
