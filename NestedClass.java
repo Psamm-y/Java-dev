b9aa3a4401425737f797824ea30072df1bd7e454
Psammy <ampadupsamuel1@gmail.com>
2024-12-23 22:07:56 +0000

    NestedClass added

---
+++ NestedClass.java
@@ -0,0 +1,18 @@
+package Javadev;
+class OuterClass {
+    int x = 10;
+
+    class InnerClass {
+        int y = 5;
+    }
+}
+
+public class NestedClass {
+    public static void main(String[] args) {
+        OuterClass myOuter = new OuterClass();
+        OuterClass.InnerClass myInner = myOuter.new InnerClass();
+        int var1= myOuter.x;
+        int var2= myInner.y;
+        System.out.println(var1/var2);
+    }
+}
