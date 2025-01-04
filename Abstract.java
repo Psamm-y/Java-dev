c012637b94a3a6bb492b7f377346bf136b226d1f
Psammy <ampadupsamuel1@gmail.com>
2025-01-04 22:35:18 +0000

    Update

--- Abstract.java
+++ Abstract.java
@@ -12,6 +12,7 @@ public abstract class Abstract {
 
         }
 
+
         
     }
 }
--- NestedClass.java
+++ NestedClass.java
@@ -1,3 +1,4 @@
+/*
 package Javadev;
 class OuterClass {
     int x = 10;
@@ -15,5 +16,23 @@ public class NestedClass {
         int var1= myOuter.x;
         int var2= myInner.y;
         System.out.println(var1/var2);
+    }
+}*/
+
+package Javadev;
+class OuterClass{
+    int x = 10;
+
+    class InnerClass{
+        int y = 5;
+    }
+    public class NestedClass{
+        public static void main(String [] args){
+            OuterClass out = new OuterClass();
+            OuterClass.InnerClass inn= out.new InnerClass();
+            int var1= out.x;
+            int var2= inn.y;
+            System.out.println(var1/var1);
+        }
     }
 }
