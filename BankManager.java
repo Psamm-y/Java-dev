eb7af3556fbe5d27711d8dc25ab32bfc0a0a42f4
Psammy <ampadupsamuel1@gmail.com>
2025-01-05 23:44:49 +0000

    Attributes

--- BankManager.java
+++ BankManager.java
@@ -1,5 +1,37 @@
 package Javadev;
 
 public class BankManager {
+    private double balance;
+    private double depositamount;
+    private String accountHoldername;
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getDepositamount() {
+        return depositamount;
+    }
+
+    public void setDepositamount(double depositamount) {
+        this.depositamount = depositamount;
+    }
+
+    public String getAccountHoldername() {
+        return accountHoldername;
+    }
+
+    public void setAccountHoldername(String accountHoldername) {
+        this.accountHoldername = accountHoldername;
+    }
+
+    
+
+
 
 }
