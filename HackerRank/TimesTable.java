bafdc320a1dbd85ccbaecf2af9ec10a061c285e9
Psammy <ampadupsamuel1@gmail.com>
2025-01-09 23:50:04 +0000

    Update

---
+++ HackerRank/TimesTable.java
@@ -0,0 +1,18 @@
+package Javadev.HackerRank;
+
+import java.util.Scanner;
+
+public class TimesTable {
+
+    public static void main (String[] args){
+        Scanner scan= new Scanner(System.in);
+        int userInput= scan.nextInt();
+        for(int i=userInput;i < userInput;i++){
+            for (int j=1;j<=10;j++){
+                int result=i*j;
+                System.out.println(i "");
+            }
+        }
+    }
+
+}
