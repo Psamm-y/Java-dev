3436ee2f92fb85490e9ceac81b08a17e01669aff
Psammy <ampadupsamuel1@gmail.com>
2025-01-09 23:57:39 +0000

    Update

--- HackerRank/TimesTable.java
+++ HackerRank/TimesTable.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class TimesTable {
 
     public static void main (String[] args){
+
+        //multiplication table
         Scanner scan= new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the number to be used the table");
         int userInput= scan.nextInt();
         for(int i=userInput;i <= userInput;i++){
             for (int j=1;j<=10;j++){
