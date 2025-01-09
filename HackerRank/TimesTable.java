c27e3e3629b46ee961155f4fef64a567ec0c2574
Psammy <ampadupsamuel1@gmail.com>
2025-01-09 23:56:01 +0000

    Update

--- HackerRank/TimesTable.java
+++ HackerRank/TimesTable.java
@@ -6,11 +6,12 @@ public class TimesTable {
 
     public static void main (String[] args){
         Scanner scan= new Scanner(System.in);
+        System.out.println("Enter the number");
         int userInput= scan.nextInt();
-        for(int i=userInput;i < userInput;i++){
+        for(int i=userInput;i <= userInput;i++){
             for (int j=1;j<=10;j++){
                 int result=i*j;
-                System.out.println(i "");
+                System.out.println(i+" x "+j+" = " +result);
             }
         }
     }
