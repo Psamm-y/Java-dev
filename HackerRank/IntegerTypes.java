eab078b37e14b673c12787d8f74d51fdccad9eeb
Psammy <ampadupsamuel1@gmail.com>
2025-01-10 18:37:04 +0000

    Add file IntegerTypes

---
+++ HackerRank/IntegerTypes.java
@@ -0,0 +1,15 @@
+package Javadev.HackerRank;
+
+import java.util.Scanner;
+
+public class IntegerTypes {
+    public static void main (String[] args){
+        System.out.println("Enter the number");
+        Scanner scan = new Scanner(System.in);
+
+        String userNumber= scan.nextLine();
+       double myLongNumber= Math.pow(2,63);
+
+       
+    }
+}
