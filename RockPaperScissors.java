00369c91ed1f75854ae16359f9af9d0809774a48
Psammy <ampadupsamuel1@gmail.com>
2025-01-14 22:55:52 +0000

    Add file RockPaperScissors

---
+++ RockPaperScissors.java
@@ -0,0 +1,10 @@
+package Javadev;
+
+public class RockPaperScissors {
+    private String userChoice;
+    private String computerChoice;
+    public static void main (String[] args){
+        System.out.println("Welcome to the Rock Paper Scissors game " +
+                "Please select from the options below");
+    }
+}
