1fc977999c98e23824a19c950842adc2ad1d5c66
Psammy <ampadupsamuel1@gmail.com>
2025-01-14 23:03:25 +0000

    Update

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -3,8 +3,13 @@ package Javadev;
 public class RockPaperScissors {
     private String userChoice;
     private String computerChoice;
+    public static String showOptions(){
+        return "1. Start game\n" +
+                "2. Cancel and exit";
+    }
     public static void main (String[] args){
         System.out.println("Welcome to the Rock Paper Scissors game " +
                 "Please select from the options below");
+
     }
 }
