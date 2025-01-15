18b5ce77330dc43b5ebbe0f1a73e56e98d4644d5
Psammy <ampadupsamuel1@gmail.com>
2025-01-15 23:33:22 +0000

    Update

--- HackerRank/Lexicography.java
+++ HackerRank/Lexicography.java
@@ -1,17 +1,23 @@
 package Javadev.HackerRank;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lexicography {
     public static void main(String[] args){
         Scanner scan= new Scanner(System.in);
-        String word1= scan.next();
-        String word2= scan.next();
-        int wordSum= word1.length()+word2.length();
+        String A= scan.next();
+        String B= scan.next();
+        int wordSum= A.length()+B.length();
 
-        String[] words= {word1,word2};
+        String[] words= {A,B};
 
-        
-
+        Arrays.sort(words);
+        if (words[0]==A){
+            System.out.println("No");
+        }else{
+            
+        }
     }
 }
