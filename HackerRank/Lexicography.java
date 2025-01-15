ef5e4b9d7f61e14453f6db87c11b4e7d619c3182
Psammy <ampadupsamuel1@gmail.com>
2025-01-15 23:10:42 +0000

    Update

--- HackerRank/Lexicography.java
+++ HackerRank/Lexicography.java
@@ -1,7 +1,11 @@
 package Javadev.HackerRank;
 
+import java.util.Scanner;
+
 public class Lexicography {
     public static void main(String[] args){
-
+        Scanner scan= new Scanner(System.in);
+        String word1= scan.next();
+        String word2
     }
 }
