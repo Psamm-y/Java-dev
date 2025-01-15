a3c9e76b4854db51590ae746baae26667b7d320e
Psammy <ampadupsamuel1@gmail.com>
2025-01-15 23:42:05 +0000

    Update

--- HackerRank/Lexicography.java
+++ HackerRank/Lexicography.java
@@ -10,14 +10,17 @@ public class Lexicography {
         String A= scan.next();
         String B= scan.next();
         int wordSum= A.length()+B.length();
-
+        System.out.println(wordSum);
         String[] words= {A,B};
-
+        System.out.println(Arrays.toString(words));
         Arrays.sort(words);
+        System.out.println(Arrays.toString(words));
         if (words[0]==A){
             System.out.println("No");
         }else{
-            
+            System.out.println("Yes");
         }
+
+        System.out.println(A.toUpperCase().charAt(0)+A.substring(1)+" "+B.toUpperCase().charAt(0)+B.substring(1));
     }
 }
