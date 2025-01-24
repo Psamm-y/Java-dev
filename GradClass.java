11df213a4965bc6549c3cf358b6dd88daa490183
Psammy <ampadupsamuel1@gmail.com>
2025-01-24 13:30:47 +0000

    Update

--- GradClass.java
+++ GradClass.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class GradClass {
     public static void main (String [] args){
         Scanner scan= new Scanner(System.in);
-        System.out.print("Please enter your overall grades in percentage: ");
+        System.out.print("Please enter CGpa ");
         double marks= scan.nextDouble();
 
-        if(marks>=90&&marks<=100){
+        if(marks>3.6&&marks<=100){
             System.out.println("You're a first class student");
+        }else if(marks>=){
+
         }
 
     }
