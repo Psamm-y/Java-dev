0698665199b1d869ea1ae9f10214ea12d09b682c
Psammy <ampadupsamuel1@gmail.com>
2025-01-25 23:27:53 +0000

    Update

--- GradClass.java
+++ GradClass.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GradClass {
     public static void main (String [] args){
         Scanner scan= new Scanner(System.in);
-        System.out.print("Please enter CGpa ");
+        System.out.print("Please enter CGpa: ");
         double gpa= scan.nextDouble();
 
         if(gpa>=3.6&&gpa<=4.00){
@@ -17,7 +17,7 @@ public class GradClass {
         }else if (gpa>=2.00||gpa<3.00){
             System.out.println("You're a third class student");
         } else if (gpa>=1.50||gpa<2.00) {
-            System.out.println();
+            System.out.println("You should've home");
         }
     }
 }
